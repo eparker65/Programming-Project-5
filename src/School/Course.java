3cc11c8cd376e280f1ae4a1dd938316cf5b1d62d
eparker65 <91712654+eparker65@users.noreply.github.com>
2022-09-12 23:17:19 -0500

    Added TestScore class and modified

--- src/School/Course.java
+++ src/School/Course.java
@@ -10,11 +10,29 @@ public class Course {
     
     public Course(String courseName){
         this.courseName = courseName;
+        studentList = new ArrayList<>();
     }
     
     public void addStudent(Student stud){
         System.out.println(stud.getName() + " has been added to " + courseName);
         studentList.add(stud);
+    }
+    
+    public double average(){
+        double total = 0;
+        for(Student studentList : studentList){
+            total+=studentList.average();
+        }
+        return total/studentList.size();
+    }
+    
+    public void roll(){
+        System.out.println("Course: " + this.courseName);
+        System.out.println("Students: ");
+        
+        for(Student studentList : this.studentList){
+            System.out.println(studentList);
+        }
     }
     
    
